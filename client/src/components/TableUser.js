2f5b5b9fcf478b9e4e99e1482e1bcfa9034340fa
isaquetdiniz <itd@cin.ufpe.br>
2020-10-30 20:02:33 -0300

    feat(configure-client): finished basic table

--- client/src/components/TableUser.js
+++ client/src/components/TableUser.js
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
-import { Table, Space } from "antd";
+import { Table, Space, Modal, Button } from "antd";
+import DataInput from "./DataInput";
 import axios from "axios";
 
 export default function TableUser() {
   const [users, setUsers] = useState();
   const [dataSource, setDataSouce] = useState();
+  const [visible, setVisible] = useState(false);
+  const [details, setDetails] = useState(false);
+  const [visibleEdit, setVisibleEdit] = useState();
 
   const getUsers = async () => {
     const res = await axios.get(`http://localhost:3001/users`);
     setUsers(res.data);
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:3001/user/${id}`);
+    getUsers();
+  };
+
+  const getInformation = async (id) => {
+    const res = await axios.get(`http://localhost:3001/user/${id}`);
+    setDetails(res.data);
   };
 
   const formatUsers = () => {
@@ -21,11 +35,6 @@ export default function TableUser() {
           nome: user.nome,
           email: user.email,
           cpf: user.cpf,
-          dataNascimento: user.dataNascimento,
-          endereco: user.endereco,
-          cep: user.cep,
-          estado: user.estado,
-          cidade: user.cidade,
         });
       });
       setDataSouce(arrayDataSource);
@@ -38,7 +47,6 @@ export default function TableUser() {
       dataIndex: "id",
       key: "id",
     },
-
     {
       title: "Nome",
       dataIndex: "nome",
@@ -55,39 +63,31 @@ export default function TableUser() {
       key: "cpf",
     },
     {
-      title: "Data de Nascimento",
-      dataIndex: "dataNascimento",
-      key: "dataNascimento",
-    },
-    {
-      title: "Endereço",
-      dataIndex: "endereco",
-      key: "endereco",
-    },
-    {
-      title: "CEP",
-      dataIndex: "cep",
-      key: "cep",
-    },
-    {
-      title: "Estado",
-      dataIndex: "estado",
-      key: "estado",
-    },
-    {
-      title: "Cidade",
-      dataIndex: "cidade",
-      key: "cidade",
-    },
-    {
       title: "Opções",
       dataIndex: "opcoes",
       key: "opcoes",
       render: (text, record) => (
         <Space size="middle">
-          <a>View</a>
-          <a>Edit</a>
-          <a>Delete</a>
+          <Button
+            type="primary"
+            onClick={() => {
+              setVisible(true);
+              getInformation(record.id);
+            }}
+          >
+            Informações
+          </Button>
+          <Button
+            onClick={() => {
+              setVisibleEdit(true);
+              getInformation(record.id);
+            }}
+          >
+            Editar
+          </Button>
+          <Button type="danger" onClick={() => deleteUser(record.id)}>
+            Deletar
+          </Button>
         </Space>
       ),
     },
@@ -104,6 +104,34 @@ export default function TableUser() {
   return (
     <>
       <Table dataSource={dataSource && dataSource} columns={columns} />
+      <Modal
+        title="Informações Detalhadas"
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+      >
+        {details && (
+          <>
+            <p>ID: {details.id}</p>
+            <p>NOME: {details.nome}</p>
+            <p>CPF: {details.cpf}</p>{" "}
+            <p>Data de Nascimento: {details.dataNascimento}</p>{" "}
+            <p>Email: {details.email}</p>
+            <p>Endereço: {details.endereco}</p>
+            <p>CEP: {details.cep}</p>
+            <p>Estado: {details.estado}</p>
+            <p>Cidade: {details.cidade}</p>
+          </>
+        )}
+      </Modal>
+      <Modal
+        title="Editar Informações"
+        visible={visibleEdit}
+        onOk={() => setVisibleEdit(false)}
+        onCancel={() => setVisibleEdit(false)}
+      >
+        <DataInput data={details} />
+      </Modal>
     </>
   );
 }
