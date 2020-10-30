f7cff3e63e2477918de8a5cfe48b9b2e68cc3a5d
Isaque Diniz <67443852+isaquetdiniz@users.noreply.github.com>
2020-10-30 12:24:58 -0300

    Revert "Feat/configure-server"

--- server/src/server.ts
+++
@@ -1,7 +0,0 @@
-import "./database/connection";
-import "reflect-metadata";
-import { app } from "./app";
-
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Ready at port ${process.env.PORT || 3001}`);
-});
